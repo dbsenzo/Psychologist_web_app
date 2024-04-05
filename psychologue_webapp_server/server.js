02e9a39c86a74dd0652a9f928ba1aa160264f113
Samy <samy.bouhamidi@efrei.net>
2024-04-05 10:29:32 +0200

    pourcentage

--- psychologue_webapp_server/server.js
+++ psychologue_webapp_server/server.js
@@ -110,6 +110,19 @@ app.get('/creneaux', (req, res) => {
   });
 });
 
+app.get('/graph/resa', (req, res) => {
+  var sql = 'SELECT (((SELECT COUNT(start) FROM resamoisactuel) * 100) / COUNT(start)) - 100 AS Pourcentage FROM resamoisprecedent;';
+  connection.query(sql, function (err, result) {
+    if (err) {
+      console.error('Erreur', err);
+      res.status(500).send('Erreur lors de la récupération des Creneaux');
+      return;
+    }
+    res.send(result);
+  });
+});
+
+
 
 
 app.listen(3000, () => console.log('Example app is listening on port 3000.'));
